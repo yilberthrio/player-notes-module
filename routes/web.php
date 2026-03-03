4fa4161731b2aad9c856226ee99e8176ec2b24a9
yilberthrio <yilberthandresgv@gmail.com>
2026-03-03 17:32:36 -0500

    rutas con la seguridad del middleware

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,20 @@
 <?php
 
+use App\Http\Controllers\PlayerController;
+use App\Models\PlayerNote;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome')->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::view('dashboard', 'dashboard')->name('dashboard');
+    Route::get('players', [PlayerController::class, 'index'])
+        ->middleware('can:players.view')
+        ->name('players.index');
+
+    Route::get('players/{player}/notes', [PlayerController::class, 'notes'])
+        ->middleware('can:viewAny,'.PlayerNote::class)
+        ->name('players.notes');
 });
 
 require __DIR__.'/settings.php';
